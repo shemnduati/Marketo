3d083e1a02ac733c158086febab1f325b0cbdfc1
Shem Nduati <nduatishem@gmail.com>
2024-10-11 06:27:22 +0300

    first stage  of project initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import About from "./components/About/About"
+import Companies from "./components/Companies/Companies"
+import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
+import Starategy from "./components/Strategy/Starategy"
 
 
 function App() {
@@ -6,6 +11,12 @@ function App() {
     <>
     <div>
       <h1>Marketo</h1>
+      <Navbar />
+      <Hero />
+      <Companies />
+      <About />
+      <Starategy />
+
     </div>
     </>
   )
---
+++ src/components/Strategy/Starategy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Starategy = () => {
+  return (
+    <div>Starategy</div>
+  )
+}
+
+export default Starategy
